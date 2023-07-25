6131d078ead97e87f85eac95efec1822b790bfb3
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2023-07-25 17:30:36 +0800

    feat: new functions

--- baby.hs
+++ baby.hs
@@ -181,3 +181,20 @@ filter' _ [] = []
 filter' p (x:xs)
   | p x = x : filter' p xs
   | otherwise = filter' p xs
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000, 99999..])
+  where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+  where isLong xs = length xs > 15
+
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
