e533698bd9e638cf9c7004302e1007b907f11d0f
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2023-08-01 10:53:30 +0800

    feat: types

--- baby.hs
+++ baby.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 -- baby's first functions
 doubleUs x y = doubleMe x + doubleMe y
 
@@ -232,3 +234,36 @@ oddSquareSum'' =
     let oddSquares = filter odd $ map (^2) [1..]
         belowLimit = takeWhile (<10000) oddSquares
     in sum belowLimit
+
+-- Types
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+
+surface :: Shape -> Float
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (x2 + b)) (Point (x2 + a) (y2 + b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+data Person = Person {
+  firstName :: String,
+  lastName :: String,
+  age :: Int,
+  height :: Float,
+  phoneNumber :: String,
+  flavor :: String
+} deriving (Show)
+
+data Car = Car {
+  company :: String,
+  model :: String,
+  year :: Int
+} deriving (Show)
